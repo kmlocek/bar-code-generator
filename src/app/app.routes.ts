105bb1c2683727952f18defdbe5f1ea5f5fe8b48
Kamil Młocek <mlocek.kamil@gmail.com>
2025-11-14 18:27:42 +0100

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LabelConfig } from './label-config/label-config';
-import { BarCodeLeaf } from './bar-code-leaf/bar-code-leaf';
-
-export const routes: Routes = [
-  { path: '', component: LabelConfig },
-  { path: 'preview', component: BarCodeLeaf },
-  { path: '**', redirectTo: '' }
-];
